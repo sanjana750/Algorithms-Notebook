11fde622f61b4a5d1dd72a31bc88ca2fad855318
sanjana750 <gr4094707@gmail.com>
2026-02-20 21:13:35 +0530

    Update GraphX.cpp

--- GraphX.cpp
+++ GraphX.cpp
@@ -1,6 +1,5 @@
-
-//COPY THE BLACKBOX, there is no need to change anything in it.
-//Check the main function at bottom for USAGE
+#include <bits/stdc++.h>
+using namespace std;
 
 //****************BLACKBOX START*****************
 //START COPYING FROM HERE
@@ -170,3 +169,31 @@ class BFS {
 };
 //END COPYING HERE
 //********************BLACKBOX END******************
+
+
+//********************USAGE (MAIN)********************
+int main() {
+    Graph g(10, true);
+
+    // add edges using raw ints
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(3, 4);
+
+    // add edges using tuples
+    g.add_edge({10, 20}, {20, 30});
+    g.add_edge({1, 2, 3}, {4, 5, 6});
+
+    BFS bfs(&g);
+
+    bfs.run(1);
+    cout << "Distance from 1 to 4: " << bfs.min_dist(4) << "\n";
+
+    bfs.run({10, 20});
+    cout << "Visited (20,30): " << bfs.is_visited({20, 30}) << "\n";
+
+    bfs.run({1, 2, 3});
+    cout << "Distance to (4,5,6): " << bfs.min_dist({4, 5, 6}) << "\n";
+
+    return 0;
+}
